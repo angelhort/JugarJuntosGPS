539c53d4531817c2a1b302c0a4d4be3c5f4bbb9b
angelhort <75445093+angelhort@users.noreply.github.com>
2022-03-01 14:38:01 +0100

    Subida proyecto

---
+++ src/backend/Artefacto/src/main/java/UCMJJGPS/JugarJuntosApplication.java
@@ -0,0 +1,13 @@
+package UCMJJGPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugarJuntosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugarJuntosApplication.class, args);
+	}
+
+}
---
+++ src/backend/Artefacto/src/test/java/UCMJJGPS/JugarJuntosApplicationTests.java
@@ -0,0 +1,13 @@
+package UCMJJGPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugarJuntosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
