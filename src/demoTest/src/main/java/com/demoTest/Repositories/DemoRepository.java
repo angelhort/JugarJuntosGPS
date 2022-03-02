77cd8acb5afef85dbd5ad462b2767e5f834489db
angelhort <75445093+angelhort@users.noreply.github.com>
2022-03-02 19:09:40 +0100

    comentaios

--- src/demoTest/src/main/java/com/demoTest/Repositories/DemoRepository.java
+++ src/demoTest/src/main/java/com/demoTest/Repositories/DemoRepository.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import com.demoTest.Entities.User;
 
+// CRUDREPOSITORY --> User es la entidad asociada a esa tabla
+//				  --> String es el tipo de la clave de la entidad
 @Repository
 public interface DemoRepository extends CrudRepository<User, String>{
 	public List<User> findAll();
