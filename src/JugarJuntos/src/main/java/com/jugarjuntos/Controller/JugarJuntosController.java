39446296207fc38387e53a5c448f9dea025c2e9f
ggasconn <guillermo.gasconsmr@gmail.com>
2022-03-19 11:25:38 +0100

    Added controller

---
+++ src/JugarJuntos/src/main/java/com/jugarjuntos/Controller/JugarJuntosController.java
@@ -0,0 +1,20 @@
+package com.jugarjuntos.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JugarJuntosController {
+
+	/**
+	 * Web index
+	 * 
+	 * @Param 	model 	model attribute holder
+	 * @return 	index view
+	 */
+	@GetMapping("/")
+	public String index(Model model) {
+		return "index";
+	}
+}
