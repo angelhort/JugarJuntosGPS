67b6be1234175334558900cac2ada3a9bcc3a508
angelhort <75445093+angelhort@users.noreply.github.com>
2022-03-21 20:32:10 +0100

    Query done

--- src/JugarJuntos/src/main/java/com/jugarjuntos/Repositories/AnuncioRepository.java
+++ src/JugarJuntos/src/main/java/com/jugarjuntos/Repositories/AnuncioRepository.java
@@ -2,6 +2,7 @@ package com.jugarjuntos.Repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,7 @@ import com.jugarjuntos.Entities.Anuncio;
 
 @Repository
 public interface AnuncioRepository extends CrudRepository<Anuncio, Long>{
+	@Query(value = "SELECT * FROM anuncio WHERE juego LIKE ?1%", nativeQuery = true)
 	public List<Anuncio> findAllByJuego(String juego);
 	public List<Anuncio> findAll();
 }
