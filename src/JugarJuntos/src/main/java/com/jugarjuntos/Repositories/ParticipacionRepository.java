14a57aadc66a79ee89e4b399dabe4e077b4c6174
Morales18 <lpejor8@gmail.com>
2022-03-22 11:49:47 +0100

    Update ParticipacionRepository.java

--- src/JugarJuntos/src/main/java/com/jugarjuntos/Repositories/ParticipacionRepository.java
+++ src/JugarJuntos/src/main/java/com/jugarjuntos/Repositories/ParticipacionRepository.java
@@ -2,11 +2,13 @@ package com.jugarjuntos.Repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.jugarjuntos.Entities.Participacion;
 import com.jugarjuntos.Entities.ParticipacionId;
 
 public interface ParticipacionRepository extends CrudRepository<Participacion , ParticipacionId> {
+	@Query(value = "SELECT * FROM participacion WHERE anuncio_id = ?1", nativeQuery = true)
 	 public List<Participacion> findAllByIdAnuncio_id(long id);
 }
