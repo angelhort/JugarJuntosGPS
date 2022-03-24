97137fd97496d1baad462a4b7775a204e5e80fe2
ggasconn <guillermo.gasconsmr@gmail.com>
2022-03-24 19:45:42 +0100

    Added integration test for seach ad

---
+++ src/JugarJuntos/src/test/java/com/jugarjuntos/integracion/BuscarAnuncioPorNombre.java
@@ -0,0 +1,60 @@
+package com.jugarjuntos.integracion;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.jugarjuntos.JugarJuntosApplication;
+
+@SpringBootTest(classes = JugarJuntosApplication.class)
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class BuscarAnuncioPorNombre {
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	private MockMvc mockMvc;
+	
+	@BeforeEach
+	public void setup() throws Exception {
+	    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+	    
+	    // Create test add
+	    this.mockMvc.perform(post("/procesarAlta?juego={game}&max_personas={max}", "TestIntegracion", 5));
+	}
+
+	@Test
+	public void loadIndexAndReturnIndexView() throws Exception {
+	    this.mockMvc.perform(get("/"))
+	    .andExpect(view().name("index"));
+	}
+	
+	@Test
+	public void loadIndexAndReturnOkCode() throws Exception {
+	    this.mockMvc.perform(get("/"))
+	    .andExpect(status().isOk())
+	    .andExpect(content().contentType("text/html;charset=UTF-8"));
+	}
+	
+	@Test
+	public void requestAdAndSuccess() throws Exception {
+	    this.mockMvc.perform(get("/"))
+	    .andExpect(status().isOk())
+	    .andExpect(content().contentType("text/html;charset=UTF-8"))
+	    .andExpect(content().string(containsString("TestIntegracion"))); 
+	}
+}
+
