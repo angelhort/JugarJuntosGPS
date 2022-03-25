0b62d1406c2483b9f30857b94acb9b54a272c635
RubnMC <rubema06@ucm.es>
2022-03-25 10:29:39 +0100

    Update SAAnuncioTest.java

--- src/JugarJuntos/src/main/java/com/jugarjuntos/ServiciosAplicacionTests/SAAnuncioTest.java
+++ src/JugarJuntos/src/main/java/com/jugarjuntos/ServiciosAplicacionTests/SAAnuncioTest.java
@@ -6,6 +6,6 @@ public interface SAAnuncioTest {
 
 	public long crearUsuario(TAnuncio tAnuncio);
 	
-	public TAnuncio getAnuncioByID(long id);
+	public TAnuncio getAnuncioByID(long id); //Devuelve un anuncio solo con los ids 1,2 y 3.
 
 }
