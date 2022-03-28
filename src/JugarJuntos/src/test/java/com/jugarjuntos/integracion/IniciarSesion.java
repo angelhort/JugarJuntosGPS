65290e6e83e083ffc6f13032ffe03d74f5233d1f
angelhort <75445093+angelhort@users.noreply.github.com>
2022-03-28 21:18:55 +0200

    TestIntegracion

--- src/JugarJuntos/src/test/java/com/jugarjuntos/integracion/IniciarSesion.java
+++ src/JugarJuntos/src/test/java/com/jugarjuntos/integracion/IniciarSesion.java
@@ -1,30 +1,32 @@
 package com.jugarjuntos.integracion;
-
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.jugarjuntos.JugarJuntosApplication;
+import com.jugarjuntos.ServiciosAplicacion.SAUsuario;
+import com.jugarjuntos.Transfers.TUsuario;
 
 @SpringBootTest(classes = JugarJuntosApplication.class)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class IniciarSesion {
 	
 	@Autowired
-	private WebApplicationContext webApplicationContext;
-
-	private MockMvc mockMvc;
+	SAUsuario saUsuario;
 	
-	@BeforeEach
-	public void setup() throws Exception {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-
-		
+	private TUsuario usuario = new TUsuario("TestIntegracion", "test@integracion.com", "pruebaTest", "testIntegracion");
+	
+	@BeforeAll
+	public void setup() throws Exception {		
+		saUsuario.altaUsuario(usuario);
 	}
-
+	
+	@Test
+	public void checkLoginUsuario() {
+		assertNotNull(saUsuario.loginUsuario(usuario));
+	}
 }
