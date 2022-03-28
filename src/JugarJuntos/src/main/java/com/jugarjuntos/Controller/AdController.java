fd4a33504fec773c242ff1b98ae10d939eb7745f
RempalagoMcWilson <72865314+RempalagoMcWilson@users.noreply.github.com>
2022-03-28 17:49:44 +0200

    Hecho que no se vea discord si no perteneces al anuncio y arreglado ver botón solicitudes

--- src/JugarJuntos/src/main/java/com/jugarjuntos/Controller/AdController.java
+++ src/JugarJuntos/src/main/java/com/jugarjuntos/Controller/AdController.java
@@ -1,6 +1,10 @@
 package com.jugarjuntos.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.jugarjuntos.Entities.Anuncio;
+import com.jugarjuntos.Entities.Participacion;
+import com.jugarjuntos.Entities.UsuarioDetalles.CustomUserDetails;
 import com.jugarjuntos.ServiciosAplicacion.SAAnuncio;
 import com.jugarjuntos.Transfers.TAnuncio;
 
@@ -47,7 +54,22 @@ public class AdController {
 	
 	@GetMapping("/detalles")
 	public String detalles(Model model, @RequestParam int id) {
-		model.addAttribute("anuncio", saAnuncio.getAnuncioByID(id));
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Long idUsuario = -1L;
+		try {
+			idUsuario = ((CustomUserDetails) principal).getId();
+		}catch(Exception e) {
+			
+		}
+		
+		model.addAttribute("idUsuario", idUsuario);
+		Anuncio anuncio = saAnuncio.getAnuncioByID(id);
+		model.addAttribute("anuncio", anuncio);
+		List<Long> participantes = new ArrayList<>();
+		for(Participacion a : anuncio.getParticipacion()) {
+			participantes.add(a.getUsuario().getId());
+		}
+		model.addAttribute("listaParticipantes", participantes);
 		return "detallesAnuncio.html";
 	}
 
