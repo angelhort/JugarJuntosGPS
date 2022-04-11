0cf27d219b82f11900d1be4d3a0323e79ad1d6c0
iQuickJP <josebarthio@gmail.com>
2022-04-11 13:37:36 +0200

    Pruebas unitarias AceptarSolicitud y comentarios en alta usuario

--- src/JugarJuntos/src/main/java/com/jugarjuntos/ServiciosAplicacionTests/SAAnuncioTest.java
+++ src/JugarJuntos/src/main/java/com/jugarjuntos/ServiciosAplicacionTests/SAAnuncioTest.java
@@ -5,7 +5,7 @@ import com.jugarjuntos.Transfers.TAnuncio;
 public interface SAAnuncioTest {
 
 	public long crearUsuario(TAnuncio tAnuncio);
-	
-	public TAnuncio getAnuncioByID(long id); //Devuelve un anuncio solo con los ids 1,2 y 3.
+
+	public TAnuncio getAnuncioByID(long id); // Devuelve un anuncio solo con los ids 1,2 y 3.
 
 }
