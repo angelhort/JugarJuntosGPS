855a3eb60a8977fb965d8f4a25eca1d848d9bead
RempalagoMcWilson <72865314+RempalagoMcWilson@users.noreply.github.com>
2022-04-14 20:46:05 +0200

    hecho vista valorar jugadores

--- src/JugarJuntos/src/main/java/com/jugarjuntos/Controller/AdController.java
+++ src/JugarJuntos/src/main/java/com/jugarjuntos/Controller/AdController.java
@@ -102,7 +102,7 @@ public class AdController {
 	}
 	
 	@GetMapping("/valorarJugadores")
-	public String valorarJugadores(Model model, @RequestParam int id) {
+	public String valorarJugadores(Model model, @RequestParam int id) {// a esto tiene que llamar el web socket
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		Long idUsuario = -1L;
 		try {
@@ -113,14 +113,40 @@ public class AdController {
 		Anuncio anuncio = saAnuncio.getAnuncioByID(id);
 		model.addAttribute("anuncio", anuncio);
 		List<Usuario> jugadoresAValorar = new ArrayList<>();
-		if(anuncio.getAnunciante().getId() != idUsuario)
+		List<Long> listaParticipantesId = new ArrayList<>();
+		if(anuncio.getAnunciante().getId() != idUsuario) {
 			jugadoresAValorar.add(anuncio.getAnunciante());
+			listaParticipantesId.add(anuncio.getAnunciante().getId());
+		}
 		for(Participacion a : anuncio.getParticipacion()) {
-			if(a.getUsuario().getId() != idUsuario)
+			if(a.getUsuario().getId() != idUsuario) {
 				jugadoresAValorar.add(a.getUsuario());
+				listaParticipantesId.add(a.getUsuario().getId());
+			}
 		}
 		model.addAttribute("listaParticipantes", jugadoresAValorar);
+		model.addAttribute("listaParticipantesId", listaParticipantesId);
 		return "valorarJugadores";
+	}
+	
+
+	@PostMapping("/valoracionJugadores")//esto es lo que devuelve la vista de valorar jugadores
+	public String valoracionJugadores(Model model, RedirectAttributes redirAttrs, @RequestParam List<Integer> listaNumEstrellas, @RequestParam List<Long> listaNumEstrellasId) {
+		
+		String aux = "";
+		for(Integer i :listaNumEstrellas) {
+			aux += i + " ";//esto se puede borrar, era para ver si funcionaba bien
+		}
+		for(Long i : listaNumEstrellasId) {
+			aux += i + " ";
+		}
+		//listaNumEstrellas es el numero de estrellas en el que a valorado el jugador
+		//listaNumEstrellasId es el id de cada jugador en el mismo orden que la del anterior
+		//ejempo: listaNumEstrellas.get(0) es la valoracion para el jug listaNumEstrellasId.get(0)
+		
+		redirAttrs.addFlashAttribute("success", aux);
+		
+		return "redirect:/";
 	}
 
 	@GetMapping("/getAnunciosPorNombre")
